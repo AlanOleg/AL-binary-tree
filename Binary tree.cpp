0024c8be19163d0e852958f724e1b6567219dc52
Alan <57073942+AlanOleg@users.noreply.github.com>
2024-05-22 02:43:44 +0300

    Add files via upload

--- Binary tree.cpp
+++ Binary tree.cpp
@@ -1,16 +1,18 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 struct Node {
     int data;
     Node* left;   // smaller
     Node* right;  // bigger
-    Node* previous;
+    Node* previous = nullptr;
 };
 
 class BSTree {
 public:
-    Node* root;
+    Node* root = nullptr;
 public:
     BSTree();
     BSTree(initializer_list<int> list);
@@ -24,46 +26,39 @@ public:
 };
 
 BSTree::BSTree() {
-    root = nullptr;
 }
 
 BSTree::BSTree(initializer_list<int> list) {
-    root = nullptr;
     for (auto i: list) {
         add_element(i);
     }
 }
 
 BSTree::~BSTree() {
-    if (root == nullptr) {
-    }
-    else {
-        Node* current = root;
-        while (not(current == root and current->left == nullptr and current->right == nullptr)) {
+    Node* current = root;
+    while (current != nullptr) {
+        while (not(current->left == nullptr and current->right == nullptr)) {
             while (current->left != nullptr) {
                 current = current->left;
             }
             while (current->right != nullptr) {
                 current = current->right;
             }
-            current = current->previous;
-            if (current->left != nullptr) {
-                delete current->left;
-                current->left = nullptr;
-            }
-            if (current->right != nullptr) {
-                if (not(current == root and (current->right->left != nullptr or current->right->right != nullptr))) {
-                    delete current->right;
-                    current->right = nullptr;
-                }
-            }
         }
-        delete current;
+        Node* to_be_deleted = current;
+        current = current->previous;
+        if (current != nullptr and current->right == to_be_deleted) {
+            current->right = nullptr;
+        }
+        else if (current != nullptr and current->left == to_be_deleted) {
+            current->left = nullptr;
+        }
+        delete to_be_deleted;
     }
 }
 
-bool BSTree::add_element(int value) {
-    Node* new_node = new Node{value, 0, nullptr, nullptr};
+bool BSTree::add_element(int data) {
+    Node* new_node = new Node{data, nullptr, nullptr, nullptr};
     if (root == nullptr) {
         root = new_node;
         return true;
@@ -92,24 +87,115 @@ bool BSTree::add_element(int value) {
     }
 }
 
+bool BSTree::delete_element(int data) {
+    Node* current = root;
+    while (not(current == nullptr)) {
+        if (data > current->data) {
+            current = current->right;
+        }
+        else if (data < current->data) {
+            current = current->left;
+        }
+        else {
+            current->right->previous = current->previous;
+            if (current->previous != nullptr) {
+                if (current == current->previous->right) {
+                    current->previous->right = current->right;
+                }
+                else {
+                    current->previous->left = current->right;
+                }
+            }
+            Node* left_edge = current->right;
+            while (left_edge->left != nullptr) {
+                left_edge = left_edge->left;
+            }
+            current->left->previous = left_edge->left;
+            left_edge->left = current->left;
+            delete current;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool BSTree::find_element(int data) {
+    Node* current = root;
+    while (not(current == nullptr)) {
+        if (data > current->data) {
+            current = current->right;
+        }
+        else if (data < current->data) {
+            current = current->left;
+        }
+        else {
+            return true;
+        }
+    }
+    return false;
+}
+
+string print_recursion(Node* current){
+    if (current == nullptr) {
+        return " ";
+    }
+    if (current->left == nullptr and current->right == nullptr) {
+        return to_string(current->data);
+    }
+    return to_string(current->data) + '(' + print_recursion(current->left) + ';' + print_recursion(current->right) + ')';
+}
+
+string print_to_file_recursion(Node* current) {
+    if (current == nullptr) {
+        return " ";
+    }
+    if (current->left == nullptr and current->right == nullptr) {
+        return to_string(current->data);
+    }
+    return to_string(current->data) + ' ' + print_to_file_recursion(current->left) +  ' ' + print_to_file_recursion(current->right);
+}
+
 void BSTree::print() {
-    cout << "test\n";
+    cout << print_recursion(root) << endl;
+}
+
+bool BSTree::save_to_file(const string& path) {
+    ofstream file_output("output.txt");
+    if (file_output.is_open()) {
+        file_output << print_to_file_recursion(root) << endl;
+        file_output.close();
+        return true;
+    }
+    else {
+        cout << "Output file didn't open." << endl;
+        return false;
+    }
+}
+
+bool BSTree::load_from_file(const string& path) {
+    ifstream file_input("input.txt");
+    if (file_input.is_open()) {
+        string data;
+        while (file_input >> data) {
+            this->add_element(stoi(data));
+        }
+        file_input.close();
+        return true;
+    }
+    else {
+        cout << "Input file didn't open." << endl;
+        return false;
+    }
 }
 
 int main() {
-    initializer_list<int> list = {1, 2, 3};
-    BSTree drevo(list);
+    ofstream file_output("output.txt");
+    file_output.close();
+    ifstream file_input("input.txt");
+    BSTree drevo;
+    drevo.load_from_file("input.txt");
+    /*initializer_list<int> list = {4, 2, 6, 1, 3, 5, 7};
+    BSTree drevo(list);*/
     drevo.print();
+    drevo.save_to_file("output.txt");
 }
-
-// 2        2^(depth-1)-1
-//1 3
-//
-//   5
-// 2   7
-//1 3 6 8
-//
-//       1
-//   1       1
-// 1   1   1   1
-//1 1 1 1 1 1 1 1
